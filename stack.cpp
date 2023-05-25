1ac5123b09837ca9c45f6888825b92320d27da89
Starisady1027 <curtischen0126@gmail.com>
2023-05-25 13:10:03 +0800

    5/25

---
+++ stack.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string a;
+	string b[1000];
+	int top=0;
+	cin>>a;
+	for (int i=0;i<a.length();i++)
+	{
+		if (a[i]=='(')
+		{
+			b[top]='(';
+			top++;
+		}
+		else if (a[i]==')')
+		{
+			b[top]
+		}
+	}
+	return 0;
+}
+
