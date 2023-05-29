aff75bc437a2b032084bbae4b0174fc6dab3cbec
Starisady1027 <curtischen0126@gmail.com>
2023-05-29 13:08:00 +0800

    5/29

--- stack.cpp
+++ stack.cpp
@@ -4,19 +4,33 @@ int main()
 {
 	string a;
 	string b[1000];
+	int n=0;
 	int top=0;
-	cin>>a;
-	for (int i=0;i<a.length();i++)
+	int x;
+	cin>>x;
+	for (int i=0;i<x;i++)
 	{
-		if (a[i]=='(')
+		cin>>a;
+		for (int i=0;i<a.length();i++)
 		{
-			b[top]='(';
-			top++;
+			if (a[i]=='(')
+			{
+				b[top]='(';
+				top++;
+			}
+			if (a[i]==')')
+			{
+				b[top]=' ';
+				top--;
+				n++;
+			}
 		}
-		else if (a[i]==')')
+		if (b[top] != ' ')
 		{
-			b[top]
+			n=0;
 		}
+		cout<<n;
+		n=0;
 	}
 	return 0;
 }
