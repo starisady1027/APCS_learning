fc3b88410a87e3eecdcd24ed8617327a364abced
Starisady1027 <curtischen0126@gmail.com>
2023-05-31 13:08:15 +0800

    5/31

--- stack.cpp
+++ stack.cpp
@@ -3,33 +3,43 @@ using namespace std;
 int main()
 {
 	string a;
-	string b[1000];
+	string b;
 	int n=0;
 	int top=0;
+	bool clear=false;
 	int x;
 	cin>>x;
 	for (int i=0;i<x;i++)
 	{
 		cin>>a;
-		for (int i=0;i<a.length();i++)
+		for (int j=0;j<a.length();j++)
 		{
-			if (a[i]=='(')
+			if (a[j]=='(')
 			{
 				b[top]='(';
 				top++;
 			}
-			if (a[i]==')')
+			if (a[j]==')' and top!=0 and b[top]=='(')
 			{
-				b[top]=' ';
 				top--;
 				n++;
 			}
 		}
-		if (b[top] != ' ')
+		for (int i=0;i<a.length();i++)
+		{
+			if (b[i]=='(' or b[i]==')')
+			{
+				clear=true;	
+			}
+		}
+		if (clear==true)
 		{
 			n=0;
 		}
 		cout<<n;
+		clear=false;
+		b="";
+		top=0;
 		n=0;
 	}
 	return 0;
