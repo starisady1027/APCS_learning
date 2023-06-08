da40aeda822b11baba3fcf378ed218e23100ca3a
Starisady1027 <curtischen0126@gmail.com>
2023-06-08 13:08:27 +0800

    6/8
    1pm

--- apcs.cpp
+++ apcs.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 int main()
 {
-	
+	char a;
+	int n=0;
+	vector<int> ans;
+	while (cin>>a)
+	{
+		if (n==5) break;
+		ans.push_back(a);
+		n++;
+	}
+	for (int i=0;i<ans.size();i++)
+	{
+		cout<<(char)ans[i]<<endl;
+	}
 	return 0;
 }
 
