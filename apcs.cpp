1a8531261ebbc0de7574d9a7e811739588de21f7
Starisady1027 <curtischen0126@gmail.com>
2023-06-09 13:01:20 +0800

    6/9

--- apcs.cpp
+++ apcs.cpp
@@ -1,19 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int operate(vector<int>ans, char o)
+{
+	sum=0;
+	for (int i=0;i<ans.size();i++)
+	{
+		sum+=ans[i];
+	}
+}
+
+
 int main()
 {
+	//f698
 	char a;
 	int n=0;
 	vector<int> ans;
-	while (cin>>a)
+	for (int i=0;i<4;i++)
 	{
-		if (n==5) break;
-		ans.push_back(a);
-		n++;
-	}
-	for (int i=0;i<ans.size();i++)
-	{
-		cout<<(char)ans[i]<<endl;
+		cin>>a;
+		if (a==int)
+		{
+			ans.push_back(a);
+		}
+		else if (a=='+')
+		{
+			ans.pop_back();
+			ans.pop_back();
+		}
+		else if (a=='-')
+		{
+		}
+		else if (a=='*')
+		{
+		}
+		else if (a=='/')
+		{
+		}
 	}
 	return 0;
 }
