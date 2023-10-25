149af5d552d16c38b4df13a0a279c886b874763e
consleq <curtischen0126@gmail.com>
2023-10-25 22:02:23 +0800

    10/25

--- apcs.cpp
+++ apcs.cpp
@@ -3,28 +3,47 @@ using namespace std;
 
 int main()
 {
-	//f698
-	string a;
-	stack <int> s;
-	while (cin>>a)
+	//m371
+	int a[100][100]={0};
+	int r, c, m, n, i, j;
+	cin >> m >> n;
+	for (int i=0;i<m;i++)
 	{
-		if (a=="+" or a=="-" or a=="*" or a=="/")
+		for (int j=0;j<n;j++)
 		{
-			int fst = s.top();
-			s.pop();
-			int sec = s.top();
-			s.pop();
-			if (a=="+") s.push(fst+sec);
-			else if (a=="-") s.push(sec-fst);
-			else if (a=="*") s.push(fst*sec);
-			else s.push(sec/fst);
-		}
-		else
-		{
-			s.push(stoi(a));
+			cin >> a[i][j];
 		}
 	}
-	cout<<s.top();
+
+	int ans = 0; bool check = true;
+
+	while (check)
+	{
+		check = false;
+		for (r=0;r<m;r++) for (c=0;c<n;c++)
+		{
+			if (a[r][c]==-1) continue;
+			for (i=r+1;i<m && a[i][c]<0;i++);
+			if (i<m && a[i][c]==a[r][c])
+			{
+				ans += a[i][c];
+				a[i][c] = -1;
+				a[r][c] = -1;
+				check = true;
+				continue;
+			}
+			for (j=c+1;j<n && a[r][j]<0;j++);
+			if (j<n && a[r][j]==a[r][c])
+			{
+				ans += a[r][j];
+				a[r][j] = -1;
+				a[r][c] = -1;
+				check = true;
+				continue;
+			}
+		}
+	}
+	cout << ans;
 	return 0;
 }
 
